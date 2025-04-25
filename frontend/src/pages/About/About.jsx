2a01e2a3b1097356da1be9694ad232083483ab07
Ibrian Danilo Giorgio <ibrian.giorgio@gmail.com>
2025-04-25 15:22:02 -0300

    refactor images and tittle and description biography

--- frontend/src/pages/About/About.jsx
+++ frontend/src/pages/About/About.jsx
@@ -25,28 +25,38 @@ const About = () => {
 
   return (
     <section className="slideshow-section">
-      <div className="slideshow-container">
-        {images.map((img, index) => (
-          <div
-            key={index}
-            className={`slide ${index === currentIndex ? "active" : ""}`}
-          >
-            <img src={img} alt={`Slide ${index}`} />
-          </div>
-        ))}
-        <div className="slideshow-controls">
-          <button onClick={prevSlide} className="control-btn">‹</button>
-          <button onClick={nextSlide} className="control-btn">›</button>
-        </div>
-        <div className="slideshow-indicators">
-          {images.map((_, index) => (
-            <span
+      <div className="slideshow-tittle">
+        <h1>Lorem Ipsum
+        </h1>
+      </div>
+        <div className="slideshow-container">
+          {images.map((img, index) => (
+            <div
               key={index}
-              className={`dot ${index === currentIndex ? "active-dot" : ""}`}
-              onClick={() => setCurrentIndex(index)}
-            />
+              className={`slide ${index === currentIndex ? "active" : ""}`}
+            >
+              <img src={img} alt={`Slide ${index}`} />
+            </div>
           ))}
+          <div className="slideshow-controls">
+            <button onClick={prevSlide} className="control-btn">‹</button>
+            <button onClick={nextSlide} className="control-btn">›</button>
+          </div>
+          <div className="slideshow-indicators">
+            {images.map((_, index) => (
+              <span
+                key={index}
+                className={`dot ${index === currentIndex ? "active-dot" : ""}`}
+                onClick={() => setCurrentIndex(index)}
+              />
+            ))}
+          </div>
         </div>
+      <div className="slideshow-text">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+
       </div>
     </section>
   );
